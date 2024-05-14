a60d50d3907eb0f9c6113efa3fb0ee737b616053
assuncaovictor <victorassuncao3382@gmail.com>
2024-05-14 14:44:36 -0300

    <Chore>: Mudanças para test

--- src/lib/components/Button/index.tsx
+++ src/lib/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Button as MuiButton, ButtonProps } from "@mui/material";
 
 export const Button = (props: ButtonProps) => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/lib/main.ts"),
 			formats: ["es"],
 		},
 		copyPublicDir: false,
+
+		rollupOptions: {
+			external: ["react", "react/jsx-runtime"],
+		},
 	},
 });
