b7c2096e637700784458f4bb75d6730baacbae09
assuncaovictor <victorassuncao3382@gmail.com>
2024-05-14 13:44:27 -0300

    <Chore>: Conseguindo rodar build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/lib/main.ts"),
+			formats: ["es"],
+		},
+		copyPublicDir: false,
+	},
+});
